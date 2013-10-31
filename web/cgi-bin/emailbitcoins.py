32518aec2951f85c9a2337dccc7c7ca0410e0e2d
mike <mike@dc.arimaa.com>
2013-10-31 11:35:18 +0300

    Initial implementation of using POST parameters

--- web/cgi-bin/emailbitcoins.py
+++ web/cgi-bin/emailbitcoins.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
+import cgi
 import sys
 import os
 import json
 import traceback
 import logging
 import simpleemail
+
 
 def readConf():
     c = open('cgi-bin/config.json', 'r').read()
@@ -14,22 +16,14 @@ def readConf():
 def sendBitcoinEmail():
 
     logging.basicConfig(filename='../logs/email.log', level=logging.DEBUG)
+    fStorage = cgi.FieldStorage()
     try:
-        conlen = int(os.environ['CONTENT_LENGTH'])
-        rawinp = sys.stdin.read(conlen)
-    except Exception:
-        rawinp = sys.stdin.read()
-    print >> sys.stderr, rawinp
-
-    if rawinp == '':
-        res = {'status':'Error', 'error': 'No input given.'}
-        print json.dumps (res)
+        len(fStorage)
+    except Exception, err:
+        print json.dumps({'status':'Error', 'error': 'No parameters specified.'})
         sys.exit()
-    logging.info("rawinp")
-    logging.info(rawinp)
-    txs = json.loads(rawinp)
-    logging.info(txs);
 
+    logging.info(fStorage)
     conf = readConf()
     emailSettings = conf.get('email','')
     if emailSettings == '':
@@ -46,51 +40,46 @@ def sendBitcoinEmail():
     if e_error != '':
         print json.dumps ({'status':'Error', 'error': e_error})
         sys.exit()
-
     emailUsername = emailSettings.get('username','')
     emailPassword = emailSettings.get('password','')
 
     e_error = ''
-    if emailUsername == '' and emailPassword != '':
-        e_error = 'SMTP username set, but not SMTP password.'
-    if emailPassword == '' and emailUsername != '':
-        e_error = 'SMTP username not set, but SMTP password is set.'
+    #if emailUsername == '' and emailPassword != '':
+    #    e_error = 'SMTP username set, but not SMTP password.'
+    #if emailPassword == '' and emailUsername != '':
+    #    e_error = 'SMTP username not set, but SMTP password is set.'
     if e_error != '':
         print json.dumps ({'status': 'Error', 'error':e_error})
         sys.exit()
 
-
-    to = txs.get('to','')
-    if to == '':
+    if "to" not in fStorage:
         res = {'status': 'Error', 'error':'No recipient given.'}
         print json.dumps(res)
         sys.exit()
+    to = fStorage["to"].value
 
-    sender = txs.get('sender','')
-    if sender == '':
+    if "sender" not in fStorage:
         res = {'status':'Error', 'error': 'No sender given.'}
         print json.dumps(res)
         sys.exit()
+    sender = fStorage['sender'].value
 
-    code = txs.get('code', '')
-    if code == '':
+    if "code" not in fStorage:
         res = {'status':'Error', 'error': 'No transaction code given.'}
         print json.dumps(res)
         sys.exit()
+    code = fStorage['code'].value
 
-    email_port = int(email_port)
     emailConfig = {'email_host': email_host, 'email_port': email_port,
                 'email_username': emailUsername, 'email_password': emailPassword}
     emailMsg = { 'sender': sender, 'to': sender,
         'subject': "You send bitcoins from EZWallet",
         'message': "You sent bitcoins to {0}. Use the code below to recover the bitcoins with EZWallet.\n\n{1}\n\n".format(to,code)}
     simpleemail.sendemail(emailConfig, emailMsg)
-
     emailMsg = { 'to': to, 'sender': sender,
             'subject': "Bitcoins from EZWallet",
             'message': "You received bitcoins from {0}. Use the following code to redeem your bitcoins with EZWallet.\n\n{1}\n\n".format(sender, code)}
     simpleemail.sendemail(emailConfig, emailMsg)
-
 
 try:
     print "Content-type: text/plain\n"
